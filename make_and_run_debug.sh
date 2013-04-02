9dd05bd530abcba0fb4ff034c97a73d2ce8e5d5f
Sho Nakatani <lay.sakura@gmail.com>
2013-04-02 16:27:25 +0900

    removed make_and_run_debug.sh from repo Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- make_and_run_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-make VERBOSE=1 && (sudo make install && sudo pkill -9 mysql ; sudo -u mysql /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf &)
-
-[ $? -ne 0 ] && exit 1
-
-sleep 5
-
-mysql -uroot mysql < support-files/uninstall.sql
-mysql -uroot mysql < support-files/install.sql
