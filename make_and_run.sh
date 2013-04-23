3a81adbdd435afc08ed238df8271d210552a2c73
Sho Nakatani <lay.sakura@gmail.com>
2013-04-23 15:16:27 +0900

    update run script Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- make_and_run.sh
+++ make_and_run.sh
@@ -3,7 +3,7 @@
 . node_conf.sh
 
 (rm -rf *.cmake CMakeFiles CMakeCache.txt ; cd ../.. ; cmake -DCMAKE_BUILD_TYPE=Release .)
-make VERBOSE=1 && (make install && pkill mysql ; $basedir/bin/mysqld --defaults-file=/home/nakatani/local/etc/my.cnf-5.6 &)
+make VERBOSE=1 && (make install && pkill mysql ; $basedir/bin/mysqld --defaults-file=$my_cnf &)
 
 [ $? -ne 0 ] && exit 1
 
