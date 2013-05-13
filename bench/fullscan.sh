53999448c109225e8030fe8e49da7856df46b6cb
Sho Nakatani <lay.sakura@gmail.com>
2013-05-13 11:52:51 +0900

    adds benchmarks Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

---
+++ bench/fullscan.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+db=/data/local/nakatani/sqlite3/group_by/10000000records.sqlite3
+query="select count(*) from large_table"
+. $(cd $(dirname $0);pwd)/common.sh
--- bench/group-by.sh
+++ bench/group-by.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
-
 db=/data/local/nakatani/sqlite3/group_by/1000000records.sqlite3
-
-cat $db > /dev/null  # read onto page cache
-
-cat <<EOF
-
--- [MySQLite] --
-EOF
-~/local/mysql-5.6/bin/mysql -v -v -u root test -e "select sqlite_db('$db'); select avg(val_col) from large_table group by key_col;"
-
-
-cat <<EOF
-
--- [SQLite] --
-EOF
-time sqlite3 $db "select avg(val_col) from large_table group by key_col;"
+query="select avg(val_col) from large_table group by key_col;"
+. $(cd $(dirname $0);pwd)/common.sh
