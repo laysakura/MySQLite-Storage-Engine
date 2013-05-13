55b38d7e203e507e7729cab35f4ccee149f3cf50
Sho Nakatani <lay.sakura@gmail.com>
2013-05-13 16:36:03 +0900

    adds join benchmark Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- bench/fullscan.sh
+++ bench/fullscan.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-db=/data/local/nakatani/sqlite3/group_by/100000000records.sqlite3
+db=/data/local/nakatani/sqlite3/bench_db//100000000records.sqlite3
 query="select count(*) from large_table"
 . $(cd $(dirname $0);pwd)/common.sh
--- bench/group-by.sh
+++ bench/group-by.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-db=/data/local/nakatani/sqlite3/group_by/1000000records.sqlite3
+db=/data/local/nakatani/sqlite3/bench_db//1000000records.sqlite3
 query="select avg(val_col) from large_table group by key_col;"
 . $(cd $(dirname $0);pwd)/common.sh
