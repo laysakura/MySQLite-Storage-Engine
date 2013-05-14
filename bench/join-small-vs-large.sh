e84bbce608d71c33b28f139e9fae40feebd7c43c
Sho Nakatani <lay.sakura@gmail.com>
2013-05-14 14:04:06 +0900

    adds DB to join Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- bench/join-small-vs-large.sh
+++ bench/join-small-vs-large.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-db=/data/local/nakatani/sqlite3/bench_db/join-small-vs-large.sqlite
+#db=/data/local/nakatani/sqlite3/bench_db/join-small-vs-large.sqlite
+db=/home/nakatani/git/mitoh/src/insert_large_table/join-debug.sqlite
 query="select count(*) from T, S where T.key_col = S.key_col;"
 . $(cd $(dirname $0);pwd)/common.sh
